b0e8cf108954f447108b3c3911ee64488093bd7e
Thuan Silva <thuansilva14@gmail.com>
2025-07-07 21:00:11 -0400

    feat: Criar Header da tela principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,29 @@
-import { Link } from "expo-router";
-import React from "react";
+import ExploreHeader from "@/components/ExploreHeader";
+import { Stack } from "expo-router";
+import React, { useMemo, useState } from "react";
 import { View } from "react-native";
 
+import ListingsBottomSheet from "@/components/ListingsBottomSheet";
+import listingsData from "../../assets/data/airbnb-listings.json";
 function Page() {
+  const items = useMemo(() => listingsData as any, []);
+  // const getoItems = useMemo(() => listingsDataGeo, []);
+  const [category, setCategory] = useState<string>("Tiny homes");
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
   return (
-    <View>
-      <Link href="/(modals)/login">Login</Link>
-      <Link href="/(modals)/booking">Booking</Link>
-      <Link href="/listing/137">Listing</Link>
+    <View style={{ flex: 1, marginTop: 185 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      {/* <Listing /> */}
+      {/* <ListingsMap listings={getoItems} /> */}
+      <ListingsBottomSheet listings={items} category={category} />
     </View>
   );
 }
